ba44d82ab3c9187ec88bda3e7e9de8a9edeb44f2
yaincoding <yaincoding@gmail.com>
2021-09-21 17:59:09 +0900

    some typos fixed

--- scripts/delete_book_index.sh
+++ scripts/delete_book_index.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #ELASTICSEARCH_HOST="http://ec2-13-209-181-246.ap-northeast-2.compute.amazonaws.com:9200/book?pretty"
-ELASTICSEARCH_HOST="http://localhost/book?pretty"
+ELASTICSEARCH_HOST="http://localhost:9200/book?pretty"
 
 curl -XDELETE $ELASTICSEARCH_HOST \
--u "${ES_USER}:${ES_PASSWORD}" \
+-u "${ES_USER}:${ES_PASSWORD}" 
