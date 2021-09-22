7cd63dedea123564c7a2bd3b923d096eb6e54a5e
yaincoding <yaincoding@gmail.com>
2021-09-22 15:48:24 +0900

    custom exception class for elasticsearch call exception

---
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/exception/BookSearchException.java
@@ -0,0 +1,12 @@
+package com.yaincoding.yacobooksapi.domain.book.exception;
+
+public class BookSearchException extends RuntimeException {
+
+	public BookSearchException() {
+		super();
+	}
+
+	public BookSearchException(String msg) {
+		super(msg);
+	}
+}
