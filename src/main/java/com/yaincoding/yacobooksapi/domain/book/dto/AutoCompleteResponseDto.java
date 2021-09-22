26a378a67c0a1e44682c0ee593293be0e27f862a
yaincoding <yaincoding@gmail.com>
2021-09-22 15:48:00 +0900

    auto complete response dto

---
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/AutoCompleteResponseDto.java
@@ -0,0 +1,15 @@
+package com.yaincoding.yacobooksapi.domain.book.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AutoCompleteResponseDto {
+	
+	private String result;
+	private List<String> titles;
+}
