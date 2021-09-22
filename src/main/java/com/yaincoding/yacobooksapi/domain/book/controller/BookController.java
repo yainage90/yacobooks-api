1ab9839c2482802664896d709350bcca90a63f1f
yaincoding <yaincoding@gmail.com>
2021-09-22 15:49:11 +0900

    1. auto completion controller added 2. global exception handler for BookSearchException added

--- src/main/java/com/yaincoding/yacobooksapi/domain/book/controller/BookController.java
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/controller/BookController.java
@@ -1,10 +1,14 @@
 package com.yaincoding.yacobooksapi.domain.book.controller;
 
+import com.yaincoding.yacobooksapi.domain.book.dto.AutoCompleteResponseDto;
 import com.yaincoding.yacobooksapi.domain.book.dto.BookSearchRequestDto;
 import com.yaincoding.yacobooksapi.domain.book.dto.BookSearchResponseDto;
 import com.yaincoding.yacobooksapi.domain.book.entity.Book;
+import com.yaincoding.yacobooksapi.domain.book.exception.BookSearchException;
 import com.yaincoding.yacobooksapi.domain.book.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +31,15 @@ public class BookController {
 	@GetMapping(value = "/search")
 	public ResponseEntity<BookSearchResponseDto> search(BookSearchRequestDto bookSearchRequestDto) {
 		return ResponseEntity.ok(bookService.search(bookSearchRequestDto));
+	}
+
+	@GetMapping(value = "/ac")
+	public ResponseEntity<AutoCompleteResponseDto> autoComplete(String query) {
+		return ResponseEntity.ok(bookService.autoComplete(query));
+	}
+
+	@ExceptionHandler(BookSearchException.class)
+	public ResponseEntity<String> handleSearchException() {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("FAIL");
 	}
 }
