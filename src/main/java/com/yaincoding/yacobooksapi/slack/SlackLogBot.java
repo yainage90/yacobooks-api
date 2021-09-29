06237051c6e96eadb5507c59296e467b4eafe43b
yaincoding <yaincoding@gmail.com>
2021-09-29 22:58:29 +0900

    Slack webhook client for error log send

---
+++ src/main/java/com/yaincoding/yacobooksapi/slack/SlackLogBot.java
@@ -0,0 +1,55 @@
+package com.yaincoding.yacobooksapi.slack;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class SlackLogBot {
+
+	private static final String SLACK_WEBHOOK_URL = System.getenv("SLACK_WEBHOOK_URL");
+
+	private static final Gson gson = new Gson();
+
+	public static void sendError(Throwable error) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		HttpPost httpPost = new HttpPost(SLACK_WEBHOOK_URL);
+		httpPost.addHeader("Content-Type", "application/json");
+
+		JsonObject body = new JsonObject();
+		body.addProperty("text", error.getMessage());
+		String bodyString = gson.toJson(body);
+		StringEntity entity = new StringEntity(bodyString, StandardCharsets.UTF_8);
+
+		httpPost.setEntity(entity);
+
+		try {
+			httpClient.execute(httpPost);
+		} catch (IOException e) {
+		}
+	}
+
+	public static void sendMessage(String msg) {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		HttpPost httpPost = new HttpPost(SLACK_WEBHOOK_URL);
+		httpPost.addHeader("Content-Type", "application/json");
+
+		JsonObject body = new JsonObject();
+		body.addProperty("text", msg);
+		String bodyString = gson.toJson(body);
+		StringEntity entity = new StringEntity(bodyString, StandardCharsets.UTF_8);
+
+		httpPost.setEntity(entity);
+
+		try {
+			httpClient.execute(httpPost);
+		} catch (IOException e) {
+		}
+	}
+}
