af22bf4fab03550c222fd314c06a6f10d1c8c60d
yaincoding <yaincoding@gmail.com>
2021-10-12 00:31:10 +0900

    chosung search, hantoeng search, engtohan search, no result enum values added

--- src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/SearchHitStage.java
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/SearchHitStage.java
@@ -2,7 +2,9 @@ package com.yaincoding.yacobooksapi.domain.book.dto;
 
 public enum SearchHitStage {
 
-	TITLE_SEARCH("title_search"), TITLE_AUTHOR_SEARCH("title_author_search");
+	TITLE_SEARCH("title_search"), TITLE_AUTHOR_SEARCH("title_author_search"), CHOSUNG_SEARCH(
+			"chosung_search"), ENG_TO_HAN_SEARCH(
+					"eng_to_han_search"), HAN_TO_ENG_SEARCH("han_to_eng_search"), NO_RESULT("no_result");
 
 	protected final String STAGE;
 
