703e7e664c7eb93815e519e330ce3b799b53c4f5
yaincoding <yaincoding@gmail.com>
2021-10-12 00:31:24 +0900

    static factory method for empty result added

--- src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/BookSearchResponseDto.java
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/BookSearchResponseDto.java
@@ -1,5 +1,6 @@
 package com.yaincoding.yacobooksapi.domain.book.dto;
 
+import java.util.Collections;
 import java.util.List;
 import com.yaincoding.yacobooksapi.domain.book.entity.Book;
 import lombok.Getter;
@@ -10,9 +11,19 @@ import lombok.ToString;
 @Setter
 @ToString
 public class BookSearchResponseDto {
-	
+
 	private String result;
 	private long totalHits;
 	private List<Book> books;
 	private String searchHitStage;
+
+	public static BookSearchResponseDto emptyResponse() {
+		BookSearchResponseDto responseDto = new BookSearchResponseDto();
+		responseDto.setResult("OK");
+		responseDto.setTotalHits(0);
+		responseDto.setBooks(Collections.emptyList());
+		responseDto.setSearchHitStage(SearchHitStage.NO_RESULT.getStage());
+
+		return responseDto;
+	}
 }
