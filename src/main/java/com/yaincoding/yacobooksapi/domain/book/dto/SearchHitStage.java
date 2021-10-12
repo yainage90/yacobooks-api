28fe1968e06289da609247d1a1f938b57557fe8e
yaincoding <yaincoding@gmail.com>
2021-10-13 01:40:38 +0900

    removed constructor and getStage() method

--- src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/SearchHitStage.java
+++ src/main/java/com/yaincoding/yacobooksapi/domain/book/dto/SearchHitStage.java
@@ -1,18 +1,5 @@
 package com.yaincoding.yacobooksapi.domain.book.dto;
 
 public enum SearchHitStage {
-
-	TITLE_SEARCH("title_search"), TITLE_AUTHOR_SEARCH("title_author_search"), CHOSUNG_SEARCH(
-			"chosung_search"), ENG_TO_HAN_SEARCH(
-					"eng_to_han_search"), HAN_TO_ENG_SEARCH("han_to_eng_search"), NO_RESULT("no_result");
-
-	protected final String STAGE;
-
-	SearchHitStage(String stage) {
-		STAGE = stage;
-	}
-
-	public String getStage() {
-		return STAGE;
-	}
+	TITLE, TITLE_AUTHOR, CHOSUNG, ENG_TO_HAN, HAN_TO_ENG, NO_RESULT;
 }
